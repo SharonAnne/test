817d3a2b3076c75eb7df8ac43fd0a89957568eef
SharonAnne <sharon.wragg@gmail.com>
2014-10-04 15:23:36 -0700

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,3 +1,9 @@
+/** 
+ * The Oooga Booga string is a modified form of Hello World
+ * asterixes are very useful for commenting out lines of text
+ * in Java.
+ */
+
 class Simple {
   public static void main(String[] args) {
     System.out.println("Ooooga booga");
